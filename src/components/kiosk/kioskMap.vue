c10ef0390978077b8e6b9421ccce97506a12c396
najaeseong1 <skwotjd5576@gmail.com>
2023-11-14 16:30:30 +0900

    2023-11-14 최영원이 말한거 다함

--- src/components/kiosk/kioskMap.vue
+++ src/components/kiosk/kioskMap.vue
@@ -1,40 +1,52 @@
 <template>
-    <div>
+  <v-app>
+    <kioskSidebar :marker="clickedMarker" v-model:drawerOpen="drawerOpen" />
+    <!-- clickedMarker 값을 kioskSidebar -->
+    컴포넌트에 props로 전달합니다
+    <v-main>
+      <div>
         <div id="map"></div>
-    </div>
+      </div>
+    </v-main>
+  </v-app>
 </template>
-  
+
 <script>
+import kioskSidebar from './kioskSidebar.vue';
 import { ref, onMounted } from 'vue';
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore';
 
 export default {
-    name: 'kioskMap',
-    setup() {
-        const latitude = ref(null);
-        const longitude = ref(null);
+  name: 'kioskMap',
+  components: {
+    kioskSidebar,
+  },
+  setup() {
+    const latitude = ref(null);
+    const longitude = ref(null);
+    const clickedMarker = ref(null); // kioskSidebar에 전달할 마커 정보
+    const drawerOpen = ref(false);  // drawer의 열림 상태를 저장할 ref 생성
 
+    onMounted(() => {
+      const script = document.createElement('script');
+      script.async = true;
+      script.src =
+        '//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=46a783bba3ff193bea6e663af44c3b84';
+      document.head.appendChild(script);
 
-        onMounted(() => {
-            const script = document.createElement('script');
-            script.async = true;
-            script.src =
-                '//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=46a783bba3ff193bea6e663af44c3b84';
-            document.head.appendChild(script);
+      script.onload = () => {
+        window.kakao.maps.load(() => {
+          const container = document.getElementById('map');
+          const options = {
+            center: new window.kakao.maps.LatLng(36.3296, 127.4332),
+            level: 1,
+          };
+          const map = new window.kakao.maps.Map(container, options);
 
-            script.onload = () => {
-                window.kakao.maps.load(() => {
-                    const container = document.getElementById('map');
-                    const options = {
-                        center: new window.kakao.maps.LatLng(36.3296, 127.4332),
-                        level: 1,
-                    };
-                    const map = new window.kakao.maps.Map(container, options);
-
-                    // Firebase 초기화
-                    const firebaseConfig = {
-                        // Firebase 설정 정보 입력
+          // Firebase 초기화
+          const firebaseConfig = {
+            // Firebase 설정 정보 입력
                         apiKey: "AIzaSyAjh-tLB9jHUFxtvf1OlFTN4TU9p0u-VTQ",
                         authDomain: "tdmk-595a3.firebaseapp.com",
                         databaseURL: "https://tdmk-595a3-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -43,81 +55,113 @@ export default {
                         messagingSenderId: "849130639775",
                         appId: "1:849130639775:web:e4d64bda00fd4a4d89adaa",
                         measurementId: "G-QXH609W16C"
-                    };
-                    const firebaseApp = initializeApp(firebaseConfig);
-                    const db = getFirestore(firebaseApp);
+          };
+          const firebaseApp = initializeApp(firebaseConfig);
+          const db = getFirestore(firebaseApp);
 
-                    // InfoWindow 인스턴스를 저장하기 위한 배열
-                    const infoWindows = [];
+          // InfoWindow 인스턴스를 저장하기 위한 배열
+          const infoWindows = [];
 
-                    // Firestore 데이터 가져오기
-                    const storeCollection = collection(db, 'stores');
-                    getDocs(storeCollection)
-                        .then((querySnapshot) => {
-                            querySnapshot.forEach((doc) => {
-                                const storeData = doc.data();
-                                const latitude = storeData.location.lat;
-                                const longitude = storeData.location.lng;
-                                const storeName = storeData.storename;
+          // Firestore 데이터 가져오기
+          const storeCollection = collection(db, 'stores');
+          const reviewCollection = collection(db, 'reviews');
+          getDocs(storeCollection)
+            .then((querySnapshot) => {
+              querySnapshot.forEach((doc) => {
+                const storeData = doc.data();
+                const stnum = storeData.Stnum; // stores 테이블 Stunum 이랑 reviews 테이블의 storedId랑 비교할 용도
+                const latitude = storeData.location.lat;
+                const longitude = storeData.location.lng;
+                const storeName = storeData.storename;
+                const image = storeData.image;
 
-                                const latLng = new window.kakao.maps.LatLng(latitude, longitude);
-                                const marker = new window.kakao.maps.Marker({
-                                    position: latLng,
-                                });
-                                marker.setMap(map);
-
-                                window.kakao.maps.event.addListener(marker, 'click', () => {
-                                    // 기존에 열려있는 InfoWindow 모두 닫기
-                                    infoWindows.forEach((infoWindow) => {
-                                        infoWindow.close();
-                                    });
-                                    const infowindow = new window.kakao.maps.InfoWindow({
-                                        content: `<div : class="info-content">${storeName}</div>`,
-                                    });
-                                    infowindow.open(map, marker);
-
-                                    // 열린 InfoWindow 인스턴스를 배열에 추가
-                                    infoWindows.push(infowindow);
-                                });
-
-                            });
-                        })
-                        .catch((error) => {
-                            console.log('Firestore 데이터를 가져오는 중 오류가 발생했습니다.', error);
-                        });
+                const latLng = new window.kakao.maps.LatLng(latitude, longitude);
+                const marker = new window.kakao.maps.Marker({
+                  position: latLng,
                 });
-            };
-        });
+                marker.setMap(map);
 
-        return {
-            latitude,
-            longitude,
-        };
-    },
+                window.kakao.maps.event.addListener(marker, 'click', () => {
+                  // 기존에 열려있는 InfoWindow 모두 닫기
+                  infoWindows.forEach((infoWindow) => {
+                    infoWindow.close();
+                  });
+                  const infowindow = new window.kakao.maps.InfoWindow({
+                    content: `<div : class="info-content">${storeName}</div>`,
+                  });
+                  infowindow.open(map, marker);
+
+                  // 열린 InfoWindow 인스턴스를 배열에 추가
+                  infoWindows.push(infowindow);
+
+                  // Firestore에서 review 데이터 가져오기
+                  const q = query(
+                    reviewCollection,
+                    where('storeId', '==', String(stnum))
+                  );
+                  const reviews = [];
+
+                  getDocs(q)
+                    .then((querySnapshot) => {
+                      // 리뷰 데이터를 저장할 배열 생성
+                      querySnapshot.forEach((doc) => {
+                        // 각 문서의 데이터 가져오기
+                        const reviewData = doc.data();
+                        reviews.push(reviewData);
+                      });
+                    })
+                    .catch((error) => {
+                      console.log(
+                        'Firestore에서 review 데이터를 가져오는 중 오류가 발생했습니다.',
+                        error
+                      );
+                    });
+                  // 정보를 clickedMarker에 추가
+                  drawerOpen.value = true;
+                  clickedMarker.value = { storeName, image, reviews};
+                });
+              });
+            })
+            .catch((error) => {
+              console.log(
+                'Firestore store 데이터를 가져오는 중 오류가 발생했습니다.',
+                error
+              );
+            });
+        });
+      };
+    });
+
+    return {
+      latitude,
+      longitude,
+      clickedMarker,
+      drawerOpen,
+    };
+  },
 };
 </script>
-  
+
 <style scoped>
 #map {
-    width: 85vw;
-    height: 85vh;
-    margin: 0 auto;
+  width: 85vw;
+  height: 85vh;
+  margin: 0 auto;
 }
 
 .button-group {
-    margin: 10px 0px;
+  margin: 10px 0px;
 }
 
 button {
-    margin: 0 3px;
+  margin: 0 3px;
 }
 
 :global(.info-content) {
-    text-align: center;
-    font-weight: bold;
-    white-space: pre-wrap;
-    max-width: 200px;
-    min-width: 200px;
+  text-align: center;
+  font-weight: bold;
+  white-space: pre-wrap;
+  max-width: 200px;
+  min-width: 200px;
 }
 </style>
-  
---
+++ src/components/kiosk/kioskSidebar.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app-bar app>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+  </v-app-bar>
+
+  <v-navigation-drawer v-model="drawer" app>
+    <!-- 여기에 사이드 바 내용을 넣습니다. -->
+    <v-list>
+      <!--marker에게 전달 받은 내용이 있으면 표시-->
+      <v-list-item v-if="marker">
+        <v-list-item-content>
+          <v-list-item-title>{{ marker.storeName }}</v-list-item-title>
+          <v-img :src="marker.image" />
+
+          <div v-if="marker.reviews.length === 0">
+            <v-list-item-subtitle>리뷰가 없습니다.</v-list-item-subtitle>
+          </div>
+          <div v-for="(review, index) in marker.reviews" :key="index">
+            <v-list-item-subtitle>이름 : {{ review.name }}</v-list-item-subtitle>
+            <v-list-item-subtitle>별점 : {{ review.rating }}</v-list-item-subtitle>
+            <v-list-item-subtitle>리뷰내용 : {{ review.comment }}</v-list-item-subtitle>
+          </div>
+
+          <!-- 여기에 필요한 정보를 추가하세요 -->
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'kioskSidebar',
+  props: {
+    marker: Object,
+    drawerOpen: Boolean,
+  },
+  data() {
+    return {
+      drawer: this.drawerOpen,
+    };
+  },
+  watch: {
+    // drawerOpen prop의 값이 변경될 때마다 drawer의 값도 변경
+    drawerOpen(newVal) {
+      this.drawer = newVal;
+    },
+    // drawer의 값이 변경될 때마다 update:drawerOpen 이벤트 발생
+    drawer(newVal) {
+      this.$emit('update:drawerOpen', newVal);
+    },
+  },
+};
+</script>
